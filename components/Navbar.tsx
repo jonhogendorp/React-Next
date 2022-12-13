1cd0635bd761bc97b0702cad7e7a7afe7e7236c9
jonhogendorp <jonhoog@live.nl>
2022-12-13 17:08:29 +0100

    next step pages

---
+++ components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link"; 
+import styles from "../styles/Home.module.css"
+
+export default function Navbar(){
+    return(
+        <nav>
+            <ul className={styles.navbar}>
+                <Link  href={"/"}>
+                    <li className={styles.li}>Home</li>
+                </Link>
+                <Link href={"/contact"}>
+                    <li className={styles.li}>Contact </li>
+                </Link>
+               
+            </ul>
+        </nav>
+    )
+}
